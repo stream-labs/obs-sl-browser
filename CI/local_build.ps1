c47e4a1d0749a3e06aea09ce7fde60675f447405
unknown <stevensoftware52@gmail.com>
2023-10-26 16:58:38 -0400

    work in progress for build script

---
+++ CI/local_build.ps1
@@ -0,0 +1,20 @@
+# Clone obs-sl-browser repository
+git clone --recursive https://github.com/stream-labs/obs-sl-browser.git
+
+# Read the obs.ver file to get the branch name
+$branchName = Get-Content -Path ".\obs-sl-browser\obs.ver" -Raw
+
+# Clone obs-studio repository with the branch name
+git clone --recursive --branch $branchName https://github.com/obsproject/obs-studio.git
+
+# Update submodules in obs-studio
+cd obs-studio
+git submodule update --init --recursive
+
+# Add new line to CMakeLists.txt in obs-studio\plugins
+$cmakeListsPath = ".\plugins\CMakeLists.txt"
+$addSubdirectoryLine = "add_subdirectory(obs-sl-browser)"
+Add-Content -Path $cmakeListsPath -Value $addSubdirectoryLine
+
+# Move obs-sl-browser folder into obs-studio\plugins
+Move-Item -Path "..\obs-sl-browser" -Destination ".\plugins\obs-sl-browser"

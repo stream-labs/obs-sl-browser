6f1b45d1a6a63fdbb411ed8f977ea43f16bb92ab
Andy Creeth <acreeth@gmail.com>
2023-11-28 15:29:13 -0800

    fix

--- CI/sign.ps1
+++ CI/sign.ps1
@@ -9,15 +9,13 @@ Write-Output "Revision is $revision"
 # Define the output file name for the 7z archive
 $archiveFileName = "slplugin-$env:SL_OBS_VERSION-$revision.7z"
 
-$tempDir = "archive"
-
 # Extract archive to a temp directory
-7z x $archiveFileName -o$tempDir
+7z x $archiveFileName -oarchive
 
 $signtool = "C:\Program Files (x86)\Microsoft SDKs\ClickOnce\SignTool\signtool.exe"
 $cert = "code-signing.p7b"
 
-Get-ChildItem -Path $tempDir -File -Recurse |
+Get-ChildItem -Path "archive" -File -Recurse |
   ForEach-Object {
     & $signtool sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /f $cert $_ | Write-Debug
   }

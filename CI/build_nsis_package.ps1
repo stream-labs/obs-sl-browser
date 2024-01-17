de504d849f2a9ac4275575a83c2057b246f16e63
unknown <stevensoftware52@gmail.com>
2024-01-17 16:17:39 -0500

    Add nsis script

---
+++ CI/build_nsis_package.ps1
@@ -0,0 +1,24 @@
+param(
+    [string]$github_workspace,
+    [string]$revision
+)
+
+$pathToFiles = "${github_workspace}\..\${revision}\build64\plugins\obs-sl-browser\RelWithDebInfo"
+
+Push-Location .\nsis
+
+$installerFileName = "slplugin-$env:SL_OBS_VERSION-$revision.exe"
+$nsisCommand = "makensis -DPACKAGE_DIR=`"$pathToFiles`" -DOUTPUT_NAME=`"$installerFileName`" package.nsi"
+Invoke-Expression $nsisCommand
+
+if ($LASTEXITCODE -ne 0) {
+    Pop-Location
+    Write-Error "NSIS compilation failed"
+    exit $LASTEXITCODE
+}
+
+# Move the installer file to the root of the GitHub workspace
+$destinationPath = Join-Path $github_workspace $installerFileName
+Move-Item $installerFileName $destinationPath -Force
+
+Pop-Location

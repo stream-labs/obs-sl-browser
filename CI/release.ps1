1fdffab8a85dc78de48ef68673df89e6c56a5abf
unknown <stevensoftware52@gmail.com>
2024-01-24 12:09:16 -0500

    wip

--- CI/release.ps1
+++ CI/release.ps1
@@ -9,7 +9,7 @@ $branchNames = $jsonContent.obsversions.PSObject.Properties.Value
 $allBranchesReady = $false
 
 # Function to check and download files
-function CheckAndDownloadFile($url, $folder, $branchName) {
+function CheckAndDownloadZip($url, $folder, $branchName) {
     $filePath = Join-Path $folder (Split-Path -Leaf $url)
     try {
         # Overwrite the file if it already exists
@@ -31,6 +31,9 @@ function CheckAndDownloadFile($url, $folder, $branchName) {
 # Variable to track successful branches
 $successfulBranches = 0
 
+# URL's for each branch's installer
+$branchInstallerUrls = @{}
+
 # Iterate through each branch
 foreach ($branchName in $branchNames) {
     # Fetch head commit SHA for the branch
@@ -44,11 +47,28 @@ foreach ($branchName in $branchNames) {
     $downloadDir = ".\.ReleaseTemp\$branchName\"
     if (-not (Test-Path $downloadDir)) { New-Item -ItemType Directory -Path $downloadDir -Force }
 
-    # Check and download file
-    $result = CheckAndDownloadFile $zipUrl $downloadDir $branchName
-    if ($result) { 
-		$successfulBranches++
-        Write-Host "$branchName is ready."
+    # Download and check zip file
+    $zipResult = CheckAndDownloadZip $zipUrl $downloadDir $branchName
+	
+	if ($zipResult){
+		# Check the installer by copying it over into /package/
+		$installerUrl = "s3://slobs-cdn.streamlabs.com/obsplugin/intermediary_packages/slplugin-$branchName-$commitSha-signed.exe"
+		$destination = "s3://slobs-cdn.streamlabs.com/obsplugin/package/slplugin-$branchName-$commitSha-signed.exe"    
+		$installerResult = $false
+			
+		try {
+			aws s3 cp $installerUrl $destination --acl public-read --metadata-directive REPLACE --cache-control "max-age=0, no-cache, no-store, must-revalidate"
+			$installerResult = $true
+		} catch {
+			Write-Host "Failed to copy or delete the installer file for $branchName"
+		}
+
+		# Check if both operations were successful
+		if ($installerResult) { 
+			$successfulBranches++
+			$branchInstallerUrls[$branchName] = "https://slobs-cdn.streamlabs.com/obsplugin/package/slplugin-$branchName-$commitSha-signed.exe"
+			Write-Host "$branchName is ready."
+		}
 	}
 }
 
@@ -71,6 +91,7 @@ function CreateJsonFile($folder, $branchName) {
 
     $jsonContent = @{}
     $jsonContent.package = $zipFile.Name
+	$jsonContent.installer = $branchInstallerUrls[$branchName]
     $jsonContent.files = $filesInBranch | Where-Object { -not $_.PSIsContainer } | ForEach-Object {
         $subPath = $_.FullName.Substring($_.FullName.IndexOf("$branchName\$branchName") + $branchName.Length * 2 + 2)
         @{
@@ -87,15 +108,15 @@ function CreateJsonFile($folder, $branchName) {
 	Write-Host $zipFilePath
 	
 	# Local environment variables, even if there are system ones with the same name, these are used for the cmd below
-	$Env:AWS_ACCESS_KEY_ID = $Env:AWS_RELEASE_ACCESS_KEY_ID
-	$Env:AWS_SECRET_ACCESS_KEY = $Env:AWS_RELEASE_SECRET_ACCESS_KEY
-	$Env:AWS_DEFAULT_REGION = "us-west-2"
+	##$Env:AWS_ACCESS_KEY_ID = $Env:AWS_RELEASE_ACCESS_KEY_ID
+	##$Env:AWS_SECRET_ACCESS_KEY = $Env:AWS_RELEASE_SECRET_ACCESS_KEY
+	##$Env:AWS_DEFAULT_REGION = "us-west-2"
 	
 	try {
-		aws s3 cp $jsonFilePath s3://slobs-cdn.streamlabs.com/obsplugin/ --acl public-read --metadata-directive REPLACE --cache-control "max-age=0, no-cache, no-store, must-revalidate"
-		aws s3 cp $zipFilePath s3://slobs-cdn.streamlabs.com/obsplugin/package/	--acl public-read --metadata-directive REPLACE --cache-control "max-age=0, no-cache, no-store, must-revalidate"
-        cfcli -d streamlabs.com purge $jsonFilePath
-        cfcli -d streamlabs.com purge $zipFilePath
+		##aws s3 cp $jsonFilePath s3://slobs-cdn.streamlabs.com/obsplugin/ --acl public-read --metadata-directive REPLACE --cache-control "max-age=0, no-cache, no-store, must-revalidate"
+		##aws s3 cp $zipFilePath s3://slobs-cdn.streamlabs.com/obsplugin/package/	--acl public-read --metadata-directive REPLACE --cache-control "max-age=0, no-cache, no-store, must-revalidate"
+        ##cfcli -d streamlabs.com purge $jsonFilePath
+        ##cfcli -d streamlabs.com purge $zipFilePath
 	}
 	catch {
 		Write-Host "S3Upload: Error: $_"

154219b1cc47b8ac276d2b4e85c2b5d9082dccbf
unknown <stevensoftware52@gmail.com>
2024-02-07 20:37:54 -0500

    wip

--- CI/release.ps1
+++ CI/release.ps1
@@ -1,14 +1,15 @@
-# URL to the JSON data
-$jsonUrl = "https://s3.us-west-2.amazonaws.com/slobs-cdn.streamlabs.com/obsplugin/obsversions.json"
+# URL to the JSON data for known OBS versions 
+$urlJsonObsVersions = "https://s3.us-west-2.amazonaws.com/slobs-cdn.streamlabs.com/obsplugin/obsversions.json"
 
 # Fetch the JSON content
-$jsonContent = Invoke-RestMethod -Uri $jsonUrl
+$jsonContent = Invoke-RestMethod -Uri $urlJsonObsVersions
 $branchNames = $jsonContent.obsversions.PSObject.Properties.Value
 
 # Boolean flag, initially set to false
 $allBranchesReady = $false
 
 # Function to check and download files
+# The purpose of the zip is to easily have the files for each seperate version so that json metadata can be made
 function CheckAndDownloadZip($url, $folder, $branchName) {
     $filePath = Join-Path $folder (Split-Path -Leaf $url)
     try {
@@ -93,10 +94,9 @@ if ($allBranchesReady) {
 	throw "Error: Not all branches are ready."
 }
 
-
 # Function to create JSON file for each branch
 function CreateJsonFile($folder, $branchName) {
-    $jsonFilePath = Join-Path $folder "$branchName.json"
+    $jsonFilePath = Join-Path $folder "rev$revNumberGoing_$branchName.json"
     $zipFile = Get-ChildItem -Path $folder -Filter "*.zip"
     $branchFolderPath = Join-Path $folder $branchName
     $filesInBranch = Get-ChildItem -Path $branchFolderPath -Recurse
@@ -125,7 +125,7 @@ function CreateJsonFile($folder, $branchName) {
 	$Env:AWS_DEFAULT_REGION = "us-west-2"
 	
 	try {
-		aws s3 cp $jsonFilePath s3://slobs-cdn.streamlabs.com/obsplugin/ --acl public-read --metadata-directive REPLACE --cache-control "max-age=0, no-cache, no-store, must-revalidate"
+		aws s3 cp $jsonFilePath s3://slobs-cdn.streamlabs.com/obsplugin/meta --acl public-read --metadata-directive REPLACE --cache-control "max-age=0, no-cache, no-store, must-revalidate"
 		
 		if ($LASTEXITCODE -ne 0) {
 			throw "AWS CLI returned a non-zero exit code: $LASTEXITCODE"
@@ -156,9 +156,53 @@ function CreateJsonFile($folder, $branchName) {
 }
 
 if ($allBranchesReady) {
+
     foreach ($branchName in $branchNames) {
 		$downloadDir = ".\.ReleaseTemp\$branchName\"
 		Write-Host "Creating json file created for $branchName..."
 		CreateJsonFile $downloadDir $branchName
+	}	
+
+	if ($LASTEXITCODE -ne 0) {
+		throw "Problem publishing new metadata for each version, not publishing new publish.josn"
+	}
+	
+	Write-Host "Formatting the next publish.json file..."
+	
+	try {
+		# URL to the JSON data for revisions
+		$urlJsonObsVersions = "https://s3.us-west-2.amazonaws.com/slobs-cdn.streamlabs.com/obsplugin/publish.json"
+
+		# Download the JSON
+		$filepathJsonPublish = ".\publish.json"
+		Invoke-WebRequest -Uri $urlJsonObsVersions -OutFile $filepathJsonPublish
+
+		# Read and parse the JSON file
+		$jsonContent = Get-Content -Path $filepathJsonPublish -Raw | ConvertFrom-Json
+
+		# Assign the value of next_rev to a variable
+		$revNumberOutgoing = $jsonContent.next_rev
+
+		# Swap around revisions and increment next_rev along the way
+		$jsonContent.last_rev = $jsonContent.next_rev
+		$jsonContent.next_rev += 1
+		$jsonContent.new_release_rev = $revNumberOutgoing
+
+		# Convert the updated object back to JSON
+		$updatedJson = $jsonContent | ConvertTo-Json
+
+		# Save the updated JSON back to the same file
+		Write-Output $updatedJson
+		$updatedJson | Out-File -FilePath $filepathJsonPublish	
+	}
+	catch {
+		throw "Error: An error occurred. Details: $($_.Exception.Message)"
+	}
+		
+	Write-Host "Uploading publish.json file..."
+	aws s3 cp $filepathJsonPublish s3://slobs-cdn.streamlabs.com/obsplugin/publish.json --acl public-read --metadata-directive REPLACE --cache-control "max-age=0, no-cache, no-store, must-revalidate"
+			
+	if ($LASTEXITCODE -ne 0) {
+		throw "On trying to upload publish.json, AWS CLI returned a non-zero exit code: $LASTEXITCODE"
 	}
 }

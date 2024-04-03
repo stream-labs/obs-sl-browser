8353ba72f45f9916ea932702e5dbc3ef74e65fee
unknown <stevensoftware52@gmail.com>
2024-04-03 12:58:50 -0400

    wip

---
+++ CI/cmake_path.ps1
@@ -0,0 +1,18 @@
+# Get the current PATH environment variable
+$currentPath = [Environment]::GetEnvironmentVariable("PATH", [EnvironmentVariableTarget]::Process)
+Write-Output $currentPath
+Write-Output "Becomes..."
+
+# Split the PATH into an array of individual paths
+$pathArray = $currentPath -split ";"
+
+# Filter out any paths that contain 'CMake'
+$filteredPathArray = $pathArray -notmatch '\\CMake\\'
+
+# Add custom CMake path at the beginning
+$customCMakePath = "C:\github\obs30\plugins\obs-sl-browser\tools\cmake"
+$newPath = $customCMakePath + ";" + ($filteredPathArray -join ";")
+
+# Set the modified PATH environment variable
+[Environment]::SetEnvironmentVariable("PATH", $newPath, [EnvironmentVariableTarget]::Process)
+Write-Output $newPath
